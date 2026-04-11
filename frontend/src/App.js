d88942e0adfde6f28cd64802dcf6d97478c09a4d
nicharr-nn <nicha.rua@gmail.com>
2026-04-11 22:08:45 +0700

    edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
-function App() {
+import Transactions from "./pages/TransactionsPage";
+import GroupsPage from "./pages/GroupsPage";
+import DashboardPage from "./pages/DashboardPage";
+import SettingsPage from "./pages/SettingPage";
+
+// NEW
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/groups" element={<GroupsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
